f3b30717eb5922926010ed0e095eb89003941368
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-07-07 20:17:39 -0300

    feat:estilização da seção receitas em destaque

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex-grow">
-      {
+      
         <section className=" bg-orange-50 py-12">
           <div className="flex flex-col gap-6 items-center container mx-auto">
             <h1 className="text-5xl font-bold">Receitas Deliciosas</h1>
@@ -12,14 +12,12 @@ export default function Home() {
             <Link className="bg-amber-500 hover:bg-amber-700 transition-colors text-white font-bold rounded-lg px-3 py-2" href="/receitas ">Ver todas as receitas</Link>
           </div>
         </section>
-      }
+      
       {/* sessão de receitas em destaque*/}
-      <section>
-        <div className="container mx-auto">
-            <h2>Receitas em destaque</h2>
-            {/* cards de receita*/}
-            <Link href="">Ver todas as receitas</Link>
-            <ChevronRight />
+      <section className="py-12">
+        <div className="flex flex-col items-center container mx-auto">
+            <h2 className="text-lg font-bold">Receitas em destaque</h2>
+            <Link className="flex text-orange-400 hover:text-orange-800 transition-colors " href="/receitas">Ver todas as receitas <ChevronRight /></Link>
         </div>
       </section>
     </main>
