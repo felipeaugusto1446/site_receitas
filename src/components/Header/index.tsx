61dc11b78ec48262e563d049ac62596b3a50de88
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-07-07 15:14:21 -0300

    feat: estrutura do header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link"
+export default function Header() {
+    return(
+        <header>
+            <Link href="/">Receitas Deliciosas</Link>
+            <nav> 
+                <Link href="/">
+                    Início
+                </Link>
+
+                <Link href="/receitas">
+                    Receitas
+                </Link>
+            </nav>
+        </header>
+    )
+}
