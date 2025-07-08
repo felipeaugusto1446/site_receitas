dc97256f3715fc39888a96ec10fa7f5470875e46
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-07-08 15:46:08 -0300

    feat: estrutura da pagina receitas

---
+++ src/app/receitas/page.tsx
@@ -0,0 +1,12 @@
+export default function receitasPage(){
+    return(
+        <main className="flex-grow py-8">
+            <div className="container mx-auto">
+                <h1 className="text-3xl font-bold">Todas as receitas</h1>
+                <div>
+                    {/*array de cards de receitas*/ }
+                </div>
+            </div>
+        </main>
+    )
+}
