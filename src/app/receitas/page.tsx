5b38650f6f5ee41e0d08a97fe4b58e5591a93f63
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-07-10 21:21:38 -0300

    feat:responsividade de todas as receitas concluída!

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -6,7 +6,7 @@ export default function receitasPage(){
         <main className="flex-grow py-8">
             <div className="container mx-auto">
                 <h1 className="text-3xl font-bold">Todas as receitas</h1>
-                <div className="grid grid-cols-3 gap-8 mt-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
                     {recipes.map((recipe)=>(        
                     <RecipeCard key={recipe.id} recipe={recipe}/>
                 ))}
