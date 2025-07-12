a65e69958e781394d09559548ec7bf09b0e99797
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-07-12 13:16:36 -0300

    feat: abrir e fechar modal de receita

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -1,17 +1,31 @@
+"use client"
+
 import RecipeCard from "@/components/RecipeCard";
+import RecipeFormModal from "@/components/RecipeFormModal";
 import { recipes } from "@/lib/data";
+import { Plus } from "lucide-react";
+import { useState } from "react";
 
 export default function receitasPage(){
+    const [isRecipeModalOpen, setIsRecipeModalOpen] = useState(false)
+
     return(
         <main className="flex-grow py-8">
             <div className="container mx-auto">
-                <h1 className="text-3xl font-bold">Todas as receitas</h1>
+                <div className="flex justify-between w-full">
+                    <h1 className="text-3xl font-bold ">Todas as receitas</h1>
+                    <button onClick={()=> setIsRecipeModalOpen(true)} className="flex items-center gap-2 px-4 py-2 rounded-lg bg-black hover:bg-gray-800 transition-colors text-white">
+                        <Plus size={16}/>
+                        Nova receita
+                    </button>
+                </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
                     {recipes.map((recipe)=>(        
                     <RecipeCard key={recipe.id} recipe={recipe}/>
                 ))}
                 </div>
             </div>
+            <RecipeFormModal isOpen={isRecipeModalOpen} onClose={()=>setIsRecipeModalOpen(false)}/>
         </main>
     )
 }
---
+++ src/components/RecipeFormModal/index.tsx
@@ -0,0 +1,19 @@
+import { Dialog, DialogContent, DialogTitle, DialogHeader } from "../ui/dialog";
+
+interface RecipeFormModalProps{
+    isOpen:boolean
+    onClose: ()=>void;
+}
+
+export default function RecipeFormModal({ isOpen, onClose }: RecipeFormModalProps)
+{
+    return(
+        <Dialog open={isOpen} onOpenChange={onClose}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Nova receita</DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
